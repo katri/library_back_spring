d54e56a5c111e89e427c8e8de13b840de6b9de32
Katri Pokats <katri.pokats@gmail.com>
2023-04-23 22:36:06 +0300

    Remove redundant classes

---
+++ src/test/java/com/example/library_back_spring/book/BookAsListServiceTest.java
@@ -0,0 +1,22 @@
+package com.example.library_back_spring.book;
+
+import com.example.library_back_spring.view.BookView;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookAsListServiceTest {
+    private final BookService bookService = new BookService();
+
+    @Test
+    void createNewBookWithAllValues() {
+//        BookView bookView = new BookView("Sipsik", 1980,"lastekirjandus", "Eno Raud");
+//
+//        BookAsList createdBookAsList = bookService.addBook(bookView);
+//
+//        assertEquals("Sipsik", createdBookAsList.getTitle());
+//        assertEquals("Eno Raud", createdBookAsList.getAuthor());
+//        assertEquals(1980, createdBookAsList.getYearPublished());
+//        assertEquals("lastekirjandus", createdBookAsList.getGenre());
+    }
+}
