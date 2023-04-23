21487a1b5d1b94997b50a15d1b61235021eba493
Katri Pokats <katri.pokats@gmail.com>
2023-04-24 00:50:41 +0300

    UserRepo changes added

--- src/main/java/com/example/library_back_spring/repository/UserRepository.java
+++ src/main/java/com/example/library_back_spring/repository/UserRepository.java
@@ -2,8 +2,13 @@ package com.example.library_back_spring.repository;
 
 import com.example.library_back_spring.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("select u from User u order by u.lastName")
+    List<User> findAllOrdered();
 }
