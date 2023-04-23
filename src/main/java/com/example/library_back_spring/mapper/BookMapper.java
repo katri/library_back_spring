4aa5075a4a0a423c695609859d793a8fcd87003f
Katri Pokats <katri.pokats@gmail.com>
2023-04-23 22:11:31 +0300

    Update BookView class & BookMapper for basic mapping

--- src/main/java/com/example/library_back_spring/mapper/BookMapper.java
+++ src/main/java/com/example/library_back_spring/mapper/BookMapper.java
@@ -2,14 +2,17 @@ package com.example.library_back_spring.mapper;
 
 import com.example.library_back_spring.entity.Book;
 import com.example.library_back_spring.view.BookView;
-import org.mapstruct.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface BookMapper {
+
+    @Mapping(target = "category", ignore = true)
     Book toEntity(BookView bookView);
 
+    @Mapping(target = "category", ignore = true)
     BookView toDto(Book book);
-
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    Book partialUpdate(BookView bookView, @MappingTarget Book book);
 }
--- src/main/java/com/example/library_back_spring/view/BookView.java
+++ src/main/java/com/example/library_back_spring/view/BookView.java
@@ -1,8 +1,5 @@
 package com.example.library_back_spring.view;
 
-import com.example.library_back_spring.entity.BookAuthor;
-import com.example.library_back_spring.entity.BookStatus;
-import com.example.library_back_spring.entity.Category;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -13,17 +10,16 @@ import java.io.Serializable;
  */
 @Data
 public class BookView implements Serializable {
-    private final Integer id;
+
     @NotNull
     private final String title;
     @NotNull
+    private final String bookAuthorFirstName;
+    private final String bookAuthorMiddleName;
+    @NotNull
+    private final String bookAuthorLastName;
+    @NotNull
     private final Integer yearPublished;
     @NotNull
-    private final String unCode;
-    @NotNull
-    private final Category category;
-    @NotNull
-    private final BookAuthor bookAuthor;
-    @NotNull
-    private final BookStatus bookStatus;
+    private final String category;
 }
