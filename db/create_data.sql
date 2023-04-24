40c18f7d911cfd3bc9a3af77c280d010715dde9e
Katri Pokats <katri.pokats@gmail.com>
2023-04-24 12:27:28 +0300

    Update data generation script

--- db/create_data.sql
+++ db/create_data.sql
@@ -1,5 +1,5 @@
 INSERT INTO public.book_author (id, first_name, middle_name, last_name) VALUES (DEFAULT, 'Eno', null, 'Raud');
-INSERT INTO public.book_author (id, first_name, middle_name, last_name) VALUES (DEFAULT, 'Oskar', null, 'Luts';)
+INSERT INTO public.book_author (id, first_name, middle_name, last_name) VALUES (DEFAULT, 'Oskar', null, 'Luts');
 
 INSERT INTO public.book_status (id, description) VALUES (DEFAULT, 'kohal');
 INSERT INTO public.book_status (id, description) VALUES (DEFAULT, 'välja laenutatud');
@@ -7,14 +7,15 @@ INSERT INTO public.book_status (id, description) VALUES (DEFAULT, 'broneeritud')
 
 INSERT INTO public."user" (id, first_name, middle_name, last_name, personal_code) VALUES (DEFAULT, 'Mari', null, 'Maasikas', 12345678);
 INSERT INTO public."user" (id, first_name, middle_name, last_name, personal_code) VALUES (DEFAULT, 'Kati', null, 'Karu', 12348678);
-INSERT INTO public."user" (id, first_name, middle_name, last_name, personal_code) VALUES (DEFAULT, 'Martin', null, 'Tamm', 45874225;)
+INSERT INTO public."user" (id, first_name, middle_name, last_name, personal_code) VALUES (DEFAULT, 'Martin', null, 'Tamm', 45874225);
 
+INSERT INTO public.category (id, name, code) VALUES (DEFAULT, 'määramata', 'MÄÄ');
 INSERT INTO public.category (id, name, code) VALUES (DEFAULT, 'lastekirjandus', 'LAS');
 INSERT INTO public.category (id, name, code) VALUES (DEFAULT, 'ulmekirjandus', 'ULM');
 INSERT INTO public.category (id, name, code) VALUES (DEFAULT, 'luule', 'LUU');
 INSERT INTO public.category (id, name, code) VALUES (DEFAULT, 'eesti kirjandus', 'EES');
 
-INSERT INTO public.book (id, title, year_published, un_code, category_id, book_author_id, book_status_id) VALUES (DEFAULT, 'Sipsik', 1962, '1234567', 1, 1, 1);
-INSERT INTO public.book (id, title, year_published, un_code, category_id, book_author_id, book_status_id) VALUES (DEFAULT, 'Roosetvaba mõõk', 1957, '1288567', 1, 1, 1);
+INSERT INTO public.book (id, title, year_published, un_code, category_id, book_author_id, book_status_id) VALUES (DEFAULT, 'Sipsik', 1962, '1289567', 1, 1, 1);
+INSERT INTO public.book (id, title, year_published, un_code, category_id, book_author_id, book_status_id) VALUES (DEFAULT, 'Roostevaba mõõk', 1957, '12885644', 1, 1, 1);
 INSERT INTO public.book (id, title, year_published, un_code, category_id, book_author_id, book_status_id) VALUES (DEFAULT, 'Naksitrallid. Esimene raamat', 1972, '1944567', 1, 1, 1);
 INSERT INTO public.book (id, title, year_published, un_code, category_id, book_author_id, book_status_id) VALUES (DEFAULT, 'Naksitrallid. Teine raamat', 1975, '8734567', 1, 1, 1);
